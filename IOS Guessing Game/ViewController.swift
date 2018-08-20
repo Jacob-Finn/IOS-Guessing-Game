8c321e6747b72d31241dd378ca04f6c66f7020b8
Jacob Finn <jacobfinn@macbook-pro-9.local>
2018-08-19 20:49:45 -0500

    Initial Commit

---
+++ IOS Guessing Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOS Guessing Game
+//
+//  Created by Jacob Finn on 8/19/18.
+//  Copyright © 2018 Jacob Finn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
